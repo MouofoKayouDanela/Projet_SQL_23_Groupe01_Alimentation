07729f05a75b914d0050a59cdb267e35dc13e4f8
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2022-10-15 21:15:13 +0100

    creation des tables
    creation de la table menu repas et ingredient_repas

---
+++ Tables/Menu.sql
@@ -0,0 +1,8 @@
+CREATE TABLE MENUS(
+    id_menu INTEGER(10),
+    intitule_menu VARCHAR(255),
+    date_debut DATE,
+    date_fin DATE,
+    budget REAL(10),
+    id_repas INTEGER(10)
+);
---
+++ Tables/Repas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE REPAS(
+    id_repas INTEGER(10),
+    nom_repas VARCHAR(20),
+    date_repas DATE,
+    prix_repas REAL(10),
+    id_menu INTEGER(10)
+);
