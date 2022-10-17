64ae085c01fff697194b676046b0fbf5d3e431b4
danelamkdap <107385969+danelamkdap@users.noreply.github.com>
2022-10-17 19:30:40 +0100

    creation  des contraintes

---
+++ Tables/Contrainte/Contrainte_Type_Abonnement.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Type_abonnements ADD
+(
+     CONSTRAINT id_abonnement NOT NULL,
+     CONSTRAINT Option_abonnement NOT NULL,
+     CONSTRAINT  Occurrence_abonnement NULL,
+     FOREIGN KEY (id_abonnement) REFERENCES Abonnements (id_abonnement),
+     PRIMARY  KEY (id_abonnement)
+);
