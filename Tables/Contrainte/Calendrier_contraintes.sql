4b71e477761336af6c2537907b2c9336747745bb
danelamkdap <107385969+danelamkdap@users.noreply.github.com>
2022-10-18 12:56:56 +0100

    modification des contraintes

--- Tables/Contrainte/Calendrier_contraintes.sql
+++ Tables/Contrainte/Calendrier_contraintes.sql
@@ -1,8 +1,7 @@
 ALTER TABLE CALENDRIER ADD
  (
-   
-   CONSTRAINT date_debut NOT NULL,CHECK (date_debut<date_fin),
    CONSTRAINT date_fin NOT NULL,
+   CONSTRAINT date_debut NOT NULL,CHECK (date_debut<date_fin),
    FOREIGN KEY (id_abonnement) REFERENCES Abonnements(id_abonnement),
    FOREIGN KEY (id_menu) REFERENCES Menus (id_menu)
 
