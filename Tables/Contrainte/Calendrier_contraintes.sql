f653b07399f28ac3c41c9908a76e652b72db8856
danelamkdap <107385969+danelamkdap@users.noreply.github.com>
2022-10-18 13:01:26 +0100

    ajout des cles primaires

--- Tables/Contrainte/Calendrier_contraintes.sql
+++ Tables/Contrainte/Calendrier_contraintes.sql
@@ -2,6 +2,7 @@ ALTER TABLE CALENDRIER ADD
  (
    CONSTRAINT date_fin NOT NULL,
    CONSTRAINT date_debut NOT NULL,CHECK (date_debut<date_fin),
+   PRIMARY key (id_abonnement,id_menu),
    FOREIGN KEY (id_abonnement) REFERENCES Abonnements(id_abonnement),
    FOREIGN KEY (id_menu) REFERENCES Menus (id_menu)
 
