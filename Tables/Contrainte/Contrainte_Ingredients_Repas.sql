b0f4105d178eed6aea15350449032a9272133671
Nikiledji Aurore <auroranikiledji@gmail.com>
2022-10-18 14:35:25 +0100

    Modification

--- Tables/Contrainte/Contrainte_Ingredients_Repas.sql
+++ Tables/Contrainte/Contrainte_Ingredients_Repas.sql
@@ -3,6 +3,7 @@ ALTER TABLE INGREDIENTS_REPAS
             CONSTRAINT id_ingredient NOT NULL,
             CONSTRAINT id_repas NOT NULL,
             CONSTRAINT quantite_ingredients NOT NULL,
-            PRIMARY KEY(id_ingredient),
-            FOREIGN KEY(id_repas) REFERENCES REPAS (id_repas)
+            FOREIGN KEY(id_repas) REFERENCES REPAS (id_repas),
+            FOREIGN KEY(id_ingredient) REFERENCES INGREDIENTS(id_ingredient),
+            PRIMARY KEY(id_ingredient, id_repas)
 );
