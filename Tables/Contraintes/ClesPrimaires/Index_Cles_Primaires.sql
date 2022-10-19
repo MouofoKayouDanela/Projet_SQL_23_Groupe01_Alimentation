bfba389c154d5ee3a1803e37606f3f260dcc9a46
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2022-10-19 22:56:29 +0100

    creation du fichier index des contraintes

---
+++ Tables/Contraintes/ClesPrimaires/Index_Cles_Primaires.sql
@@ -0,0 +1,38 @@
+PROMPT************** appellation de toutes les contraintes de la cles primaires***************
+
+PROMPT**************Contrainte Abonnements***************
+@@CpAbonnements
+
+PROMPT**************Contrainte Calendrier***************
+@@CpCalendrier
+
+PROMPT**************Contrainte Historique***************
+@@CpHistorique
+
+PROMPT**************Contrainte informations_utilisateur***************
+@@CpInformations_utilisateur
+
+PROMPT**************Contrainte CpIngredients_repas***************
+@@CpIngredients_repas
+
+PROMPT**************Contrainte Ingredients***************
+@@CpIngredients
+
+PROMPT**************Contrainte Menu***************
+@@CpMenu
+
+PROMPT**************Contrainte Notifications***************
+@@CpNotifications
+
+PROMPT**************Contrainte Repas***************
+@@CpRepas
+
+PROMPT**************Contrainte Suivi***************
+@@CpSuivi
+
+PROMPT**************Contrainte Type_abonnements***************
+@@CpTypeAbonnements
+
+PROMPT**************Contrainte Utilisateurs***************
+@@CpUtilisateurs
+
