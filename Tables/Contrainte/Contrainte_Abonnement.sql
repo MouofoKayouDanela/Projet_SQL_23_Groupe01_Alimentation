747e1ff58778554c9065a557470018b7862f9263
Mouofo Kayou Danela <107385969+danelamkdap@users.noreply.github.com>
2022-10-19 09:38:09 +0100

    modification des chemins d'acces

--- Tables/Contrainte/Contrainte_Abonnement.sql
+++ Tables/Contrainte/Contrainte_Abonnement.sql
@@ -1,6 +1,6 @@
 ALTER TABLE ABONNEMENTS ADD
 (
-    CONSTRAINT id_abonnement NOT NULL,UNIQUE ,
+    CONSTRAINT id_abonnement NOT NULL UNIQUE ,
     CONSTRAINT nom_abonnement NOT NULL,
     CONSTRAINT date_fin NOT NULL,
     CONSTRAINT date_debut NOT NULL, CHECK (date_debut<date_fin),
