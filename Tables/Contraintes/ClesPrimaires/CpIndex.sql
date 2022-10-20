64a3b286360abb0ea418738177062263463b5588
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2022-10-20 13:17:28 +0100

    modification du nom d fichier index et redefinition de la cle de la table suivie

--- Tables/Contraintes/ClesPrimaires/CpIndex.sql
+++ Tables/Contraintes/ClesPrimaires/CpIndex.sql
@@ -1,38 +1,38 @@
 PROMPT************** appellation de toutes les contraintes de la cles primaires***************
-
+PROMPT
 PROMPT**************Contrainte Abonnements***************
 @@CpAbonnements
-
+PROMPT
 PROMPT**************Contrainte Calendrier***************
 @@CpCalendrier
-
+PROMPT
 PROMPT**************Contrainte Historique***************
 @@CpHistorique
-
+PROMPT
 PROMPT**************Contrainte informations_utilisateur***************
 @@CpInformations_utilisateur
-
+PROMPT
 PROMPT**************Contrainte CpIngredients_repas***************
 @@CpIngredients_repas
-
+PROMPT
 PROMPT**************Contrainte Ingredients***************
 @@CpIngredients
-
+PROMPT
 PROMPT**************Contrainte Menu***************
 @@CpMenu
-
+PROMPT
 PROMPT**************Contrainte Notifications***************
 @@CpNotifications
-
+PROMPT
 PROMPT**************Contrainte Repas***************
 @@CpRepas
-
+PROMPT
 PROMPT**************Contrainte Suivi***************
 @@CpSuivi
-
+PROMPT
 PROMPT**************Contrainte Type_abonnements***************
 @@CpTypeAbonnements
-
+PROMPT
 PROMPT**************Contrainte Utilisateurs***************
 @@CpUtilisateurs
 
