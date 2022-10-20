fcf572af2add98baa86570e076674036e877e84b
Mouofo Kayou Danela <107385969+danelamkdap@users.noreply.github.com>
2022-10-20 13:49:10 +0100

    Update VnIndex.sql

--- Tables/Contraintes/ValeursNulles/VnIndex.sql
+++ Tables/Contraintes/ValeursNulles/VnIndex.sql
@@ -2,36 +2,36 @@ PROMPT *************** AJOUT DES CONTRAINTES DE VALEURS NULLES ***************
 PROMPT
 PROMPT *************** DE LA TABLE ABONNEMENT ***************
 @@VnAbonnements
+PROMPT
 PROMPT *************** DE LA TABLE CALENDRIER ***************
-PROMPT
 @@VnCalendrier
+PROMPT
 PROMPT *************** DE LA TABLE HISTORIQUE ***************
-PROMPT
 @@VnHistorique
+PROMPT
 PROMPT *************** DE LA TABLE INFORMATION_UTILISATEUR ***************
-PROMPT
 @@VnInformations_Utilisateur
+PROMPT
 PROMPT *************** DE LA TABLE INGREDIENTS_REPAS ***************
-PROMPT
 @@VnIngredients_repas
+PROMPT
 PROMPT *************** DE LA TABLE INGREDIENTS ***************
-PROMPT
 @@VnIngredients
+PROMPT
 PROMPT *************** DE LA TABLE MENU ***************
-PROMPT
 @@VnMenu
+PROMPT
 PROMPT *************** DE LA TABLE NOTIFICATIONS ***************
-PROMPT
 @@VnNotifications
-PROMPT *************** DE LA TABLE REPAS ***************
-PROMPT 
+PROMPT
+PROMPT *************** DE LA TABLE REPAS *************** 
 @@VnRepas
+PROMPT
 PROMPT *************** DE LA TABLE SUIVI ***************
-PROMPT
 @@VnSuivi
+PROMPT
 PROMPT *************** DE LA TABLE TYPE_ABONNEMENT ***************
-PROMPT
 @@VnTypeAbonnementS
-PROMPT *************** DE LA TABLE UTILISATEURS ***************
 PROMPT
+PROMPT *************** DE LA TABLE UTILISATEURS ***************
 @@VnUtilisateurs
