90c1e2d26a57a1b829bf93e49aba149c78e9434c
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-10-20 04:05:39 +0100

    Création de l'Index des contraintes de domaine

---
+++ Tables/Contraintes/VerificationsValeurs/VvIndex.sql
@@ -0,0 +1,15 @@
+PROMPT************* Ajout des contraites de domaine ***************
+PROMPT************* Cas de la table Abonnements **************
+@@VvAbonnements
+PROMPT************* cas de la table Calendrier **************
+@@VvCalendrier
+PROMPT************* cas de la table Informations_utilisateur *************
+@@VvInformations_utilisateur
+PROMPT************* cas de la table Ingredients_repas *************
+@@VvIngredients_repas
+PROMPT************* cas de la table Menu **************
+@@VvMenu
+PROMPT************* cas de la table Repas **************
+@@VvRepas
+PROMPT************* cas de la table Suivi **************
+@@VvUtilisateur
