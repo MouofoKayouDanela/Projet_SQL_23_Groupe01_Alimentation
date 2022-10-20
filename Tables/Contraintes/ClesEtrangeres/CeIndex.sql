f02b72e943f9b1d31f18ce78a6c96de612d5cf4a
Nikiledji Aurore <auroranikiledji@gmail.com>
2022-10-20 13:30:39 +0100

    Fichier des contraintes de cles etrangeres des tables

---
+++ Tables/Contraintes/ClesEtrangeres/CeIndex.sql
@@ -0,0 +1,41 @@
+PROMT*************** Ajout des contraintes des cles etrangeres***************
+
+PROMT ***************De la table Abonnements***************
+@@CeAbonnements
+PROMT
+
+PROMT ***************De la table Calendrier***************
+@@CeCalendrier
+PROMT
+
+PROMT ***************De la table Historique***************
+@@CeHistorique
+PROMT
+
+PROMT ***************De la table Informations_utilisateur***************
+@@CeInformations_utilisateur
+PROMT
+
+PROMT ***************De la table Ingredients_Repas***************
+@@CeIngredients_Repas
+PROMT
+
+PROMT ***************De la table Menu***************
+@@CeMenu
+PROMT
+
+PROMT ***************De la table CeRepas***************
+@@CeRepas
+PROMT
+
+PROMT ***************De la table Suivi***************
+@@CeSuivi
+PROMT
+
+PROMT ***************De la table TypeAbonnements***************
+@@CeTypeAbonnements
+PROMT
+
+PROMT ***************De la table Utilisateurs***************
+@@CeUtilisateurs
+PROMT
