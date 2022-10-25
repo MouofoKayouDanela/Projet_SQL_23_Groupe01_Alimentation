e80d1ad723fd92802f11f3c9f0152d274c0c8324
Mouofo Kayou Danela <107385969+danelamkdap@users.noreply.github.com>
2022-10-25 16:47:25 +0100

    modifications sur les tables repas ingredients et menus  et repas

--- Tables/Contraintes/VerificationsValeurs/VvIndex.sql
+++ Tables/Contraintes/VerificationsValeurs/VvIndex.sql
@@ -9,14 +9,8 @@ PROMPT
 PROMPT*************** De la table Informations_utilisateur ************
 @@VvInformations_utilisateur
 PROMPT
-PROMPT*************** De la table Ingredients_repas **************
-@@VvIngredients_repas
-PROMPT
-PROMPT*************** De la table Menu ***************
-@@VvMenu
-PROMPT
 PROMPT*************** De la table Repas_Menus ***************
-@@Vv_repas_menu
+@@VvRepas_Menu
 PROMPT
 PROMPT*************** De la table Suivi ***************
 @@VvUtilisateurs
--- Tables/Contraintes/VerificationsValeurs/VvIngredients_repas.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE INGREDIENTS_REPAS
-ADD CONSTRAINT chk_ingredients_repas_quantite
-CHECK (quantite_ingredients >= 0);
