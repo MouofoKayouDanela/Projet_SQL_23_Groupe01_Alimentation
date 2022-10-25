6b74671e7c5215159fc4b9927a3564beb085254c
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2022-10-25 20:44:48 +0100

    insertion des donnees dans la table InsAbonnements, InsCalendrier,InsType_Abonnements

--- Insertions/InsType_Abonnements.sql
+++ Insertions/InsType_Abonnements.sql
@@ -1,3 +1,94 @@
 INSERT INTO
-Type_Abonnements(id_abonnement,option_abonnement,occurrence_abonnement,frais_abonnement,)
-VALUES();
+Type_Abonnements(id_abonnement,option_abonnement,frais_abonnement,occurrence_abonnement)
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+VALUES(
+    'AB001',
+    'GRATUIT',
+    '0',
+    '1'
+);
+
+VALUES(
+    'AB002',
+    'MENSUEL',
+    '15000',
+);
+VALUES(
+    'AB002',
+    'MENSUEL',
+    '15000',
+);
+VALUES(
+    'AB002',
+    'MENSUEL',
+    '15000',
+);
+VALUES(
+    'AB002',
+    'MENSUEL',
+    '15000',
+);
+VALUES(
+    'AB002',
+    'MENSUEL',
+    '15000',
+);
