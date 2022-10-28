18803016584032c4a6a045bc357e11cfb2fa4053
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2022-10-28 20:30:42 +0100

    Modification du mode d'insertion dans la table calendrier

--- Tables/Contraintes/ClesEtrangeres/CeCalendrier.sql
+++ Tables/Contraintes/ClesEtrangeres/CeCalendrier.sql
@@ -1,11 +1,7 @@
 ALTER TABLE CALENDRIER
 ADD(
         CONSTRAINT ce_calendrier_id_abonnement
-        FOREIGN KEY (id_abonnement) REFERENCES ABONNEMENTS(id_abonnement),
+        FOREIGN KEY (id_abonnement, date_debut_abon, id_utilisateur) REFERENCES ABONNEMENTS(id_abonnement, date_debut_abon, id_utilisateur),
         CONSTRAINT ce_calendrier_id_menu
-        FOREIGN KEY (id_menu) REFERENCES MENUS(id_menu),
-        CONSTRAINT ce_calendrier_id_utilisation
-        FOREIGN KEY (id_utilisateur) REFERENCES MENUS(id_utilisateur),
-        CONSTRAINT ce_calendrier_date_debut_abonn
-        FOREIGN KEY (date_debut_abon) REFERENCES MENUS(date_debut_abon)
+        FOREIGN KEY (id_menu) REFERENCES MENUS(id_menu)
 );
--- Tables/Contraintes/ClesPrimaires/CpCalendrier.sql
+++ Tables/Contraintes/ClesPrimaires/CpCalendrier.sql
@@ -1,3 +1,3 @@
 ALTER TABLE CALENDRIER
 ADD CONSTRAINT cp_calendrier
-PRIMARY KEY(id_abonnement, id_menu, date_debut_menu);
+PRIMARY KEY(id_abonnement, id_utilisateur, id_menu, date_debut_menu);
